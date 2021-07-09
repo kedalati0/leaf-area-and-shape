93fccee2d5ba4762dca581965401fbab853e5c27
Marcel Vaz <mcvaz@ucla.edu>
2021-07-09 14:37:10 -0300

    update results - LAI, TLA etc

--- BM2.R
+++ BM2.R
@@ -12,29 +12,19 @@ library(coda) # MCMC
 
 # Load data input for the model
 data = read.csv("DATA.csv",as.is=T) # import raw data
-table(data$lblade) 
-data = subset(data,lblade=="simple") # select only simple-leafed species
 head(data) # check
 
 # Load priors for the model -> ##FIX!## (deal with it later...)
-refD = log(median(data$dbh)); refM = log(median(data$aldm)) # median DBH and LDM
-preD = log(seq(1,4,l=20)); preM = seq(-4.6,2.3,len=20); preSLA = seq(-1,1,len=5) # DBH, LDM, and SLA used for predictions
-data$sla = data$larea/data$aldm # calculate SLA
-mod2 = lm(log(data$larea)~log(data$aldm)) # leaf area as a function of leaf dry mass
-data$sla2 = mod2$residuals
-cor.test(log(data$aldm),log(data$larea)) # 0.96
-cor.test(log(data$aldm),log(data$sla)) # -0.54
-cor.test(log(data$aldm),data$sla2) # 0
-cor.test(log(data$sla),data$sla2) # 0.85
-plot(sla2~sla,data,log="x") # conclusion: SLA estimated as residuals is tightly correlated to SLA calculated the old-fashioned way, but it has the benefit of being independent from leaf mass
+refD = median(data$dbh); refM = median(data$aldm) # median DBH and LDM
+preD = seq(1,4,l=20); preM = exp(seq(log(0.01),log(10),len=20)); preSLA = seq(-1,1,len=5) # DBH, LDM, and SLA used for predictions
 
 # Basic model input and parameters
 dat = list(
   N = nrow(data), #S = length(unique(data$sp)), sp = as.numeric(factor(data$sp)), # metadata
   C = log(data$tnlc), # number of leaf units (individual level)
-  M = log(data$aldm), SLA = data$sla2, # leaf dry mass and SLA
-  D = log(data$dbh)-refD, L = data$cii-1, # covariates (individual level)
-  preD = preD-refD, preM = preM-refM, preSLA = preSLA, # predictions' parameters
+  M = log(data$aldm)-log(refM), SLA = data$sla2, # predictors (leaf dry mass and SLA)
+  D = log(data$dbh)-log(refD), L = data$cii-1, # covariates (DBH and CII)
+  preD = log(preD)-log(refD), preM = log(preM)-log(refM), preSLA = preSLA, # predictions' parameters
   pripa = diag(rep(.001,5)), prim = rep(0,5)  # priors for sampling error model (alphas)
   )
 
@@ -97,6 +87,8 @@ res = coda.samples(mod,params,n.iter=6680*4,thin=20)
 dim(res[[1]]) # one of the chains
 res2 = as.matrix(res)
 dim(res2)
+save(res2,file="POST.RData")
+
 
 ### Results
 # Parameter estimates (alphas)
@@ -113,150 +105,194 @@ hist(a3,main="alpha[3]",xlab="L effect on LN"); abline(v=0,col=2,lwd=2,lty=3)
 hist(a4,main="alpha[4]",xlab="M effect on LN"); abline(v=0,col=2,lwd=2,lty=3)
 hist(a5,main="alpha[5]",xlab="SLA effect on LN"); abline(v=0,col=2,lwd=2,lty=3)
 
-## Checking data fit
+## Graphical parameters
+labSLA = expression(paste("SLA (",mm^2,".",mg^-1,")"))
+labTLA = expression(paste("Total leaf area (",m^2,")"))
+labMLA = expression(paste("Mean leaf area (",cm^2,")"))
+labD = "Stem diameter (cm)"
+labL = "Crown illumination index"
+labLDM = "Leaf dry mass (g)"
+labLN = "Leaf number"
+
+### Checking data fit
+## GROWTH CURVES
 # Growth curves (as a function of light)
-curve(exp(m1 + m2*(log(x)-refD)),from=1,to=4,ylab="Number of leaves per plant",xlab="DBH (cm)",
+curve(exp(m1 + m2*(log(x)-log(refD))),from=1,to=4,ylab="Number of leaves per plant",xlab="",
       ylim=range(data$tnlc),log="xy",lwd=.5) # L = 1
 points(tnlc~dbh,data,col=gray(.5,.5),pch=16)
-curve(exp(m1 + m2*(log(x)-refD) + m3*1),add=T,from=1,to=4,lwd=1) # L = 2
-curve(exp(m1 + m2*(log(x)-refD) + m3*2),add=T,from=1,to=4,lwd=2) # L = 3
+curve(exp(m1 + m2*(log(x)-log(refD)) + m3*1),add=T,from=1,to=4,lwd=1) # L = 2
+curve(exp(m1 + m2*(log(x)-log(refD)) + m3*2),add=T,from=1,to=4,lwd=2) # L = 3
 legend("topleft",c("1","2","3"),title="L",bty="n",lwd=c(.5,1,2))
 
 # Growth curves (as a function of leaf mass)
-curve(exp(m1 + m2*(log(x)-refD)),from=1,to=4,ylab="Number of leaves per plant",xlab="DBH (cm)",
+curve(exp(m1 + m2*(log(x)-log(refD))),from=1,to=4,ylab="",xlab="DBH (cm)",
       ylim=range(data$tnlc),log="xy") # leaf mass = 1 g
 points(tnlc~dbh,data,col=gray(.5,.5),pch=16)
-curve(exp(m1 + m2*(log(x)-refD) + m4*2.3),add=T,from=1,to=4,lwd=2) # leaf mass = 10 g
-curve(exp(m1 + m2*(log(x)-refD) + m4*-2.3),add=T,from=1,to=4,lwd=.5) # leaf mass = 0.1 g
+curve(exp(m1 + m2*(log(x)-log(refD)) + m4*(log(10)-log(refM))),add=T,from=1,to=4,lwd=2) # leaf mass = 10 g
+curve(exp(m1 + m2*(log(x)-log(refD)) + m4*(log(.1)-log(refM))),add=T,from=1,to=4,lwd=.5) # leaf mass = 0.1 g
 legend("topleft",c("0.1","1.0","10"),title="Leaf mass (g)",bty="n",lwd=c(.5,1,2))
 
 # Growth curves (as a function of SLA)
-curve(exp(m1 + m2*(log(x)-refD)),from=1,to=4,ylab="Number of leaves per plant",xlab="DBH (cm)",
+curve(exp(m1 + m2*(log(x)-log(refD))),from=1,to=4,ylab="",xlab="",
       ylim=range(data$tnlc),log="xy") # SLA = 0 (?)
 points(tnlc~dbh,data,col=gray(.5,.5),pch=16)
-curve(exp(m1 + m2*(log(x)-refD) + m5*1),add=T,from=1,to=4,lwd=2) # SLA = 1 (?)
-curve(exp(m1 + m2*(log(x)-refD) + m5*-1),add=T,from=1,to=4,lwd=0.5) # SLA = -1 (?)
-legend("topleft",c("-1","0","1"),title="SLA",bty="n",lwd=c(.5,1,2))
+curve(exp(m1 + m2*(log(x)-log(refD)) + m5*1),add=T,from=1,to=4,lwd=2) # SLA = 1 (?)
+curve(exp(m1 + m2*(log(x)-log(refD)) + m5*-1),add=T,from=1,to=4,lwd=0.5) # SLA = -1 (?)
+legend("topleft",c("5","13","35"),title=labSLA,bty="n",lwd=c(.5,1,2))
 
+
+## TRADE-OFF
 # Observed tradeoff (as a function of D)
-curve(exp(m1 + m4*log(x) + m2*(log(2)-refD)),from=.05,to=10,ylab="Number of leaves per plant",xlab="Leaf mass (g)",
+curve(exp(m1 + m4*(log(x)-log(refM)) + m2*(log(2)-log(refD))),from=.05,to=10,ylab="Number of leaves per plant",xlab="",
       ylim=range(data$tnlc),log="xy") # L = 1; D = 1.79 cm; SLA = 0
 points(tnlc~aldm,data,col=gray(.5,.5),pch=16)
-curve(exp(m1 + m4*log(x) + m2*(log(1)-refD)),add=T,from=.05,to=10,lwd=.5) # D = 
-curve(exp(m1 + m4*log(x) + m2*(log(3)-refD)),add=T,from=.05,to=10,lwd=2) # D = 
+curve(exp(m1 + m4*(log(x)-log(refM)) + m2*(log(1)-log(refD))),add=T,from=.05,to=10,lwd=.5) # D = 
+curve(exp(m1 + m4*(log(x)-log(refM)) + m2*(log(3)-log(refD))),add=T,from=.05,to=10,lwd=2) # D = 
 legend("topright",c("1","2","3"),title="DBH (cm)",bty="n",lwd=c(.5,1,2))
 
 # Observed tradeoff (as a function of L) -> n.s.
-curve(exp(m1 + m4*log(x)),from=.05,to=10,ylab="Number of leaves per plant",xlab="Leaf mass (g)",
+curve(exp(m1 + m4*(log(x)-log(refM))),from=.05,to=10,ylab="",xlab="Leaf mass (g)",
       ylim=range(data$tnlc),log="xy",lwd=.5) # L = 1; D = 1.79 cm; SLA = 0
 points(tnlc~aldm,data,col=gray(.5,.5),pch=16)
-curve(exp(m1 + m4*log(x) + m3*1),add=T,from=.05,to=10,lwd=1) # L = 2
-curve(exp(m1 + m4*log(x) + m3*2),add=T,from=.05,to=10,lwd=2) # L = 3
+curve(exp(m1 + m4*(log(x)-log(refM)) + m3*1),add=T,from=.05,to=10,lwd=1) # L = 2
+curve(exp(m1 + m4*(log(x)-log(refM)) + m3*2),add=T,from=.05,to=10,lwd=2) # L = 3
 legend("topright",c("1","2","3"),title="L",bty="n",lwd=c(.5,1,2))
 
 # Observed tradeoff (as a function of SLA)
-curve(exp(m1 + m4*log(x)),from=.05,to=10,ylab="Number of leaves per plant",xlab="Leaf mass (g)",
+curve(exp(m1 + m4*(log(x)-log(refM))),from=.05,to=10,ylab="",xlab="",
       ylim=range(data$tnlc),log="xy") # L = 1; D = 1.79 cm; SLA = 0
 points(tnlc~aldm,data,col=gray(.5,.5),pch=16)
-curve(exp(m1 + m4*log(x) + m5*1),add=T,from=.05,to=10,lwd=2) # SLA = 1 (?)
-curve(exp(m1 + m4*log(x) + m5*-1),add=T,from=.05,to=10,lwd=.5) # SLA = -1 (?)
-legend("topright",c("-1","0","1"),title="SLA",bty="n",lwd=c(.5,1,2))
+curve(exp(m1 + m4*(log(x)-log(refM)) + m5*1),add=T,from=.05,to=10,lwd=2) # SLA = 1 (?)
+curve(exp(m1 + m4*(log(x)-log(refM)) + m5*-1),add=T,from=.05,to=10,lwd=.5) # SLA = -1 (?)
+legend("topright",c("5","13","35"),title=labSLA,bty="n",lwd=c(.5,1,2))
+
 
 ## Predicted tradeoffs
 # For plants with the same size (D = 1.79 cm) growing in the shade (L = 1) and with a median SLA (SLA = 0)
 tmp = exp(res2[,paste0("pre[",6,",",1:length(preM),",",3,"]")]) # medians
 tt = apply(tmp,2,quantile,c(.025,.975)) # credible intervals
-plot(apply(tmp,2,median),x=exp(preM),log="xy",type="l",xlab="Average mass per leaf (g)",ylab="Number of leaves per plant")
-polygon(y=c(tt[1,],rev(tt[2,])),x=exp(c(preM,rev(preM))),col=grey(.5,.5),border=NA)
+plot(apply(tmp,2,median),x=(preM),log="xy",type="l",xlab="Average mass per leaf (g)",ylab="Number of leaves per plant")
+polygon(y=c(tt[1,],rev(tt[2,])),x=(c(preM,rev(preM))),col=grey(.5,.5),border=NA)
 points(tnlc~aldm,data,col=gray(.5,.5),pch=16)
 
 ## Predicted vs. observed (model fit)
-pred = exp(m1 + m2*(log(data$dbh)-refD) + m3*(data$cii-1) + m4*log(data$aldm) + m5*data$sla2)
+pred = exp(m1 + m2*(log(data$dbh)-log(refD)) + m3*(data$cii-1) + m4*(log(data$aldm)-log(refM)) + m5*data$sla2)
 plot(pred~data$tnlc,log="xy");abline(a=0,b=1)
 R2 = 1-sum((log(pred)-log(data$tnlc))^2)/sum((log(data$tnlc)-mean(log(data$tnlc)))^2); R2
 res = numeric()
 for(i in 1:nrow(tmp)){
-  pred = exp(a1[i] + a2[i]*(log(data$dbh)-refD) + a3[i]*(data$cii-1) + a4[i]*log(data$aldm) + a5[i]*data$sla2)
+  pred = exp(a1[i] + a2[i]*(log(data$dbh)-log(refD)) + a3[i]*(data$cii-1) + a4[i]*(log(data$aldm)-log(refM)) + a5[i]*data$sla2)
   res[i] = 1-sum((log(pred)-log(data$tnlc))^2)/sum((log(data$tnlc)-mean(log(data$tnlc)))^2)
   }
 hist(res,main="CI of R^2")  
 quantile(res,c(.025,.5,.975)) # credible interval
 
-## Total leaf surface area
-parea = exp(mod2$coef[1]+mod2$coef[2]*log(data$aldm))
-plot(parea,data$larea,log="xy"); abline(0,1)
-# as a function of leaf size, but constant SLA
-curve(exp(m1+m2*(log(x)-refD))*exp(mod2$coef[1]+mod2$coef[2]*log(1))/10000,
-      ylab="Total leaf surface (m2)",xlab="DBH (cm)")
-curve(exp(m1+m2*(log(x)-refD)+m4*log(10))*exp(mod2$coef[1]+mod2$coef[2]*log(10))/10000,
-      lwd=2,add=T)
-curve(exp(m1+m2*(log(x)-refD)+m4*log(.1))*exp(mod2$coef[1]+mod2$coef[2]*log(.1))/10000,
-      lwd=.5,add=T)
-legend("topleft",title="Leaf mass (g)",c("0.1","1.0","10"),lwd=c(.5,1,2),bty="n")
-# as a function of SLA, but constant leaf size
-curve(exp(m1+m2*(log(x)-refD))*exp(mod2$coef[1]+mod2$coef[2]*log(1))/10000,
-      ylab="Total leaf surface (m2)",xlab="DBH (cm)")
-curve(exp(m1+m2*(log(x)-refD)+m5*-1)*(exp(mod2$coef[1]+mod2$coef[2]*log(1))+exp(-1))/10000,
-      lwd=.5,add=T)
-curve(exp(m1+m2*(log(x)-refD)+m5*1)*(exp(mod2$coef[1]+mod2$coef[2]*log(1))+exp(1))/10000,
-      lwd=2,add=T)
-legend("topleft",title="SLA",leg=c(-1,0,1),lwd=c(.5,1,2),bty="n")
-# as a function of light
-curve(exp(m1+m2*(log(x)-refD))*exp(mod2$coef[1]+mod2$coef[2]*log(1))/10000,
-      ylab="Total leaf surface (m2)",xlab="DBH (cm)",lwd=.5)
-curve(exp(m1+m2*(log(x)-refD)+m3*1)*exp(mod2$coef[1]+mod2$coef[2]*log(1))/10000,
-      lwd=1,add=T)
-curve(exp(m1+m2*(log(x)-refD)+m3*2)*exp(mod2$coef[1]+mod2$coef[2]*log(1))/10000,
-      lwd=2,add=T)
-legend("topleft",title="Light",leg=1:3,lwd=c(.5,1,2),bty="n")
-# as a function of leaf size and SLA
-curve(exp(m1+m2*(log(x)-refD))*exp(mod2$coef[1]+mod2$coef[2]*log(1))/10000,
-      ylab="Total leaf surface (m2)",xlab="DBH (cm)")
-curve(exp(m1+m2*(log(x)-refD)+m4*log(10)+m5*-1)*(exp(mod2$coef[1]+mod2$coef[2]*log(10))+exp(-1))/10000,
-      lty=2,add=T)
-curve(exp(m1+m2*(log(x)-refD)+m4*log(.1)+m5*1)*(exp(mod2$coef[1]+mod2$coef[2]*log(.1))+exp(1))/10000,
-      lty=3,add=T)
-legend("topleft",leg=c("M = 1; SLA = 0","M = 10; SLA = -1","M = 0.1; SLA = 1"),lty=1:3,bty="n")
 
-# tradeoff with predicted values (standardized saplings: same DBH, CII, and SLA)
-tmp = aggregate(cbind(aldm,sla2,larea)~sp,data,median)
-tmp$aldm = log(tmp$aldm)
-tmp$larea = log(tmp$larea)
-tmp2 = matrix(NA,nrow=length(a1),ncol=nrow(tmp))
+## Total leaf surface area
+data$tla = data$mla*data$tnlc # observed total leaf area
+plot(tla/10000~dbh,data,log="xy")
+plot(tla/10000~cii,data,log="xy")
+plot(tla/10000~aldm,data,log="xy")
+plot(tla/10000~sla,data,log="xy")
+plot(tla/10000~sla2,data,log="y")
+plot(tla/10000~mla,data,log="xy")
+mod = lm(log(data$mla)~log(data$aldm)) # SLA model
+mod2 = lm(log(data$sla)~(data$sla2)) # SLA2->SLA model
+exp(mod2$coef[1]+mod2$coef[2]*1) # SLA when SLA2 = 1
+exp(mod2$coef[1]+mod2$coef[2]*0) # SLA when SLA2 = 0 
+exp(mod2$coef[1]+mod2$coef[2]*-1) # SLA when SLA2 = -1
+pretla = exp(m1+m2*(log(data$dbh)-log(refD))+m3*(data$cii-1)+m4*(log(data$aldm)-log(refM))+m5*data$sla2)*data$aldm/exp(mod2$coef[1]+mod2$coef[2]*data$sla2)  # predicted TLA
+plot(data$tla,pretla,log="xy")
+summary(lm(log(pretla)~log(data$tla)))
+tfun = function(d=refD,l=1,m=refM,s=0){
+  exp(m1+m2*(log(d)-log(refD))+m3*(l-1)+m4*(log(m)-log(refM))+m5*s)* # total number of leaves (predicted)
+    exp(mod$coef[1]+mod$coef[2]*log(m))/10000
+  }
+
+# as a function of leaf mass
+plot(tla/10000~(dbh),data,col=gray(.5,.5),pch=16,ylab=labTLA,xlab="",ylim=range(data$tla/10000),xlim=c(.5,4.5),log="y")
+curve(tfun(d=x,m=.1),lwd=.5,add=T)
+curve(tfun(d=x,m=10),lwd=2,add=T)
+curve(tfun(d=x,m=1),lwd=1,add=T)
+legend("bottomright",title="Leaf mass (g)",c("0.1","1.0","10"),lwd=c(.5,1,2),bty="n")
+
+# as a function of SLA
+plot(tla/10000~(dbh),data,col=gray(.5,.5),pch=16,ylab=labTLA,xlab="",ylim=range(data$tla/10000),xlim=c(.5,4.5),log="y")
+curve(tfun(d=x,s=-1),lwd=2,add=T)
+curve(tfun(d=x,s=1),lwd=.5,add=T)
+curve(tfun(d=x,s=0),lwd=1,add=T)
+legend("bottomright",c("5","13","35"),title=labSLA,bty="n",lwd=c(.5,1,2))
+
+# as a function of light
+plot(tla/10000~(dbh),data,col=gray(.5,.5),pch=16,ylab=labTLA,xlab="",ylim=range(data$tla/10000),xlim=c(.5,4.5),log="y")
+curve(tfun(d=x,l=1),lwd=.5,add=T)
+curve(tfun(d=x,l=2),lwd=2,add=T)
+curve(tfun(d=x,l=3),lwd=1,add=T)
+legend("bottomright",title="Light",leg=1:3,lwd=c(.5,1,2),bty="n")
+
+
+### Predictions
+## interspecific tradeoff with predicted values (standardized saplings: same DBH, CII, and SLA)
+tmp = aggregate(cbind(aldm,sla,sla2,mla)~sp,data,median) # median leaf size (mass and area) and SLA per species
+#tmp$aldm = log(tmp$aldm); tmp$mla = log(tmp$mla) # log-transform leaf area and SLA
+head(tmp); nrow(tmp) # check
+tmp2 = matrix(NA,nrow=length(a1),ncol=nrow(tmp)); colnames(tmp2) = tmp$sp # create empty matrix
 for(j in 1:nrow(tmp)){
   for(i in 1:length(a1)){
-    tmp2[i,j] = exp(a1[i]+a4[i]*tmp[j,"aldm"]+a5[i]*tmp[j,"sla2"])
+    tmp2[i,j] = exp(a1[i]+a4[i]*(log(tmp[j,"aldm"])-log(refM))+a5[i]*tmp[j,"sla2"]) # estimated number of leaves (ENL) for median-sized saplings growing in the shade
     }
   }
-tmp2[1:5,1:5]
-tmp$enl = apply(tmp2,2,median)
-plot(enl~aldm,tmp,log="y")
-cor(log(tmp$enl),tmp$aldm) # median correlation
-tmp3 = numeric()
-for(i in 1:length(a1)){ tmp3[i] = cor(log(tmp2[i,]),tmp$aldm) }
+tmp2[1:5,1:3] # check
+tmp$enl = apply(tmp2,2,median) # estimated number of leaves
+
+# ELN vs. LDM
+plot(enl~aldm,tmp,log="xy",xlab=labLDM,ylab="Predicted leaf number",pch=16,col=grey(.5,.5),main="Interspecific trade-off")
+cor.test(log(tmp$enl),log(tmp$aldm)) # median correlation R=-0.98
+tmp3 = numeric(); for(i in 1:length(a1)){ tmp3[i] = cor(log(tmp2[i,]),log(tmp$aldm)) } # simulate correlations
+quantile(tmp3,c(.025,.975)) # CI for R
+legend("topright","R = -0.98***",bty="n")
+
+# ELN vs. SLA and SLA2
+plot(enl~sla,tmp,log="xy"); cor.test(log(tmp$enl),log(tmp$sla)) # SLA
+plot(enl~sla2,tmp,log="y"); cor.test(log(tmp$enl),tmp$sla2) # SLA2
+
+# ELN vs. MLA
+plot(enl~mla,tmp,log="xy"); cor.test(log(tmp$enl),log(tmp$mla)) 
+tmp3 = numeric(); for(i in 1:length(a1)){ tmp3[i] = cor(log(tmp2[i,]),log(tmp$mla)) }
 quantile(tmp3,c(.025,.975))
 
-plot(enl~sla2,tmp,log="y")
-cor.test(log(tmp$enl),tmp$sla2) # median correlation
 
-plot(enl~larea,tmp,log="y")
-plot(enl~exp(larea),tmp)
-cor.test(log(tmp$enl),tmp$larea) # median correlation
-tmp3 = numeric()
-for(i in 1:length(a1)){ tmp3[i] = cor(log(tmp2[i,]),tmp$larea) }
+## Total leaf area: large leaf advantage
+tmp4 = matrix(NA,nrow=length(a1),ncol=nrow(tmp)); colnames(tmp2) = tmp$sp # create empty matrix
+for(i in 1:nrow(tmp)){tmp4[,i] = tmp2[,i]*tmp$mla[i]/10000} # simulate total leaf area 
+tmp$etla = apply(tmp4,2,median) # estimated total leaf area (m^2)
+head(tmp) # check
+
+# TLA vs. LDM
+plot(etla~aldm,tmp,log="xy",xlab=labLDM,ylab=labTLA,pch=16,col=grey(.5,.5),main="Large leaf advantage")
+cor.test(log(tmp$etla),log(tmp$aldm)) # median correlation R = 0.42***
+tmp3 = numeric(); for(i in 1:length(a1)){ tmp3[i] = cor(log(tmp4[i,]),tmp$aldm) } # simulate correlations
+quantile(tmp3,c(.025,.975)) # CI95% = [-0.18;0.62]
+sum(tmp3>0)/length(tmp3) # P(R>0) = 91%
+
+# TLA vs. SLA and SLA2
+plot(etla~sla,tmp,log="xy"); cor.test(log(tmp$etla),log(tmp$sla)) # SLA
+plot(etla~sla2,tmp,log="y"); cor.test(log(tmp$etla),tmp$sla2) # SLA2
+
+# ELN vs. MLA
+plot(etla~mla,tmp,log="xy",xlab=labMLA,ylab=labTLA,pch=16,col=grey(.5,.5),main="Large leaf advantage"); cor.test(log(tmp$etla),log(tmp$mla))
+legend("topleft","R = 0.65***",bty="n")
+tmp3 = numeric(); for(i in 1:length(a1)){ tmp3[i] = cor(log(tmp4[i,]),log(tmp$mla)) }
 quantile(tmp3,c(.025,.975))
 
 
-# trade-off between low leaf overshading (LAI) and total leaf area?
-data$tla = data$larea*data$tnlc
-data$lai = data$tla/data$cpa
-plot(lai~larea,data,log="xy",xlab="Leaf area (cm^2)",ylab="LAI")
-cor.test(data$lai,data$larea)
-summary(lm(lai~larea,data))
-legend("bottomright","R = 0.29 \n p<0.001",bty="n")
-cor.test(data$lai,data$aldm)
+## trade-off between low leaf overshading (LAI) and total leaf area?
+# observed
+data$lai = data$tla/data$cpa # leaf area index
+plot(lai~mla,data,log="xy",xlab=labMLA,ylab="LAI",main="Small leaf advantage",pch=16,col=grey(.5,.5))
+cor.test(log(data$lai),log(data$mla))
+legend("bottomright","R = 0.32***",bty="n")
+cor.test(log(data$lai),log(data$aldm))
+cor.test(log(data$lai),(data$sla2))
 
-
-# center SLA too?
-# rethink total leaf area predictions as a function of SLA...
 
--- CODE_DataPrep.R
+++ CODE_DataPrep.R
@@ -16,31 +16,37 @@ head(area)
 aarea = aggregate(larea~id,area,mean) # average leaf area per plant
 head(aarea); nrow(aarea) # 2798
 
+# Calculate the sum of sampled leaf area per plant
+aarea2 = aggregate(larea~id,area,sum) # average leaf area per plant
+head(aarea2); nrow(aarea2) # 2798
+aarea = merge(aarea,aarea2,by="id") # merge with avg. area
+names(aarea)[2:3] = c("mla","ssla")
+
 # Sapling architecture
 arch = read.csv("RAW_architect.csv") # load raw architecture data
 arch= subset(arch, select = c("trip","mm","dd","plantID", "dbh", "tnlc", "cii", "nl", "lblade", "cd1", "cd2")) # simplify architecture data frame
 names(arch)[4] = "id"
 arch$cpa = pi*(arch$cd1/2)*(arch$cd2/2)
-head(arch)
+head(arch); nrow(arch) # 1105
 
 # Leaf mass 
 mass = read.csv("RAW_traits.csv") # load raw trait data frame
 mass = subset(mass,select=c("plantID","ldm"))
 names(mass)[1] = "id"
-head(mass)
+head(mass); nrow(mass) # 1114
 
 # Species identities and initial dbh
 mao = read.csv("RAW_mao.csv") # plot data
 mao$sp = paste(mao$gen,mao$epi) # add species column
 names(mao)[which(names(mao)=="dbh")] = "dbh0"
-head(mao)
+head(mao); nrow(mao) # 158497
 
 # Putting things together...
 am = merge(arch,mass,by="id"); nrow(am) # merge arch and mass (individual level data) # 1090
 am = merge(am,mao[,c("id","sp","dbh0")],by="id"); nrow(am) # add species identities and plant initial size # 1086
-am = merge(am,aarea) # add mean leaf area per plant # 970
+am = merge(am,aarea); nrow(am) # add mean leaf area per plant # 970
 am = am[order(am$sp,am$id),] # organize by species, then by id
-nrow(am) # 970
+
 
 ### Fix other minor errors
 am[am$sp=="Inga lateriflora" & am$lblade=="simple","sp"] = "NA NA"
@@ -61,9 +67,36 @@ am$ldm = am$ldm/100 # convert mass to g
 ### FIX! #### nl refers to number of leaves scanned, not leaf units!
 am$aldm = am$ldm/am$nl # calculates mean leaf dry mass per plant
 
+# select only simple-leafed saplings
+table(am$lblade)
+am = subset(am,lblade=="simple") # select only simple-leafed species
+nrow(am) # 494
+
+# SLA ###FIX!### -> check why there is a difference in the two ways of calculating SLA (mean vs. sum)
+mod = lm(log(am$mla)~log(am$aldm)) # leaf area as a function of leaf dry mass (mean of sampled leaves)
+mod2 = lm(log(am$ssla)~log(am$ldm)) # leaf area as a function of leaf dry mass (sum of sampled leaves)
+summary(mod) # R^2 = 0.92 -> best model
+summary(mod2) # R^2 = 0.75
+plot(mla~aldm,am,log="xy")
+plot(ssla~ldm,am,log="xy")
+am$sla = (am$mla/100^2)/(am$aldm/1000) # calculate SLA (m^2/kg)
+am$sla2 = mod$residuals # SLA calculated as the residuals of the area~mass function
+cor.test(log(am$aldm),log(am$mla)) # 0.96
+cor.test(log(am$aldm),log(am$sla)) # -0.53
+cor.test(log(am$aldm),am$sla2) # 0
+cor.test(log(am$sla),am$sla2) # 0.85 -> basically the same information
+plot(sla2~sla,am,log="x") # conclusion: SLA estimated as residuals is tightly correlated to SLA calculated the old-fashioned way, but it has the benefit of being independent from leaf mass
+
+# plots
+labA = expression(paste("Leaf area (",cm^2,")"))
+hist(log10(am$aldm),xlab="Leaf mass (g)",main="",ylim=c(0,120),xaxt="n"); axis(side=1,at=c(-1,0,1),lab=c(0.1,1,10))
+hist(log10(am$mla),xlab=labA,main="",ylim=c(0,120),xaxt="n"); axis(side=1,at=c(1,2,3),lab=c(10,100,1000))
+hist(log10(am$tnlc),xlab="Total # of leaves",main="",ylim=c(0,120),xaxt="n"); axis(side=1,at=c(1,2,3),lab=c(10,100,1000))
+
+
 #############
-head(am); nrow(am) # 652 plants
-length(unique(am$sp)) # 377 species
+head(am); nrow(am) # 494 plants
+length(unique(am$sp)) # 286 species
 hist(table(am$sp)) # sample size per species
 write.csv(am,"DATA.csv",row.names=F)
 
